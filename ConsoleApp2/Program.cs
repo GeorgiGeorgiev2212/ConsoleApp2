532b5deb894495210487ff35ce056be87359d129
Georgi Georgiev <Georgi Georgiev@DESKTOP-9FI4TKH>
2025-02-07 15:21:49 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿int grade = int.Parse(Console.ReadLine());
+
+if (grade >= 90)
+{
+    Console.WriteLine("Excellent");
+}
+else if (grade >= 80 && grade < 90)
+{
+    Console.WriteLine("Very Good");
+}
+else if (grade >= 80 && grade < 90)
+{
+    Console.WriteLine("Good");
+}
+else if (grade < 70)
+{
+   
+    Console.WriteLine("Bad");
+
+}
+
